55f2a6af9810ee4cae46e95f1092ca72bdeb35be
Binod Bist <thebinodbist@gmail.com>
2025-11-18 16:38:55 +0545

    ...

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
    return (
     <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-4xl font-bold text-blue-500">Hello NativeWind!</Text>
+      <Text className="text-4xl font-bold text-cyan-300">Hello NativeWind!</Text>
     </View>
   );
 }
