737d52d0c30605bd438ab6bc0e304da0809d5328
Binod Bist <thebinodbist@gmail.com>
2025-11-18 17:39:19 +0545

    ...

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -5,7 +5,9 @@ import { tokenCache } from '@clerk/clerk-expo/token-cache'
 export default function RootLayout() {
   return (
     <ClerkProvider  tokenCache={tokenCache}>
-      <Stack />
+      <Stack >
+        <Stack.Screen name="(auth)" options={{headerShown:false}}/>
+      </Stack>
     </ClerkProvider>
   );
 }
