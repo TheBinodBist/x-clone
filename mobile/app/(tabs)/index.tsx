113c1db9658a6de194234198026632df5c5f2bd4
Binod Bist <thebinodbist@gmail.com>
2025-11-19 11:48:40 +0545

    ...

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Index = () => {
   return (
-    <View>
+    <View className='flex-1 justify-center'>
       <Text>Index</Text>
     </View>
   )
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -3,10 +3,14 @@ import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { Stack } from "expo-router";
 import "./global.css"
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 export default function RootLayout() {
-  return <ClerkProvider tokenCache={tokenCache}>
-      <Stack  screenOptions={{headerShown:false}}/>
-    </ClerkProvider>
+  return
+  <ClerkProvider tokenCache={tokenCache}>
+    <QueryClientProvider client={QueryClient}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </QueryClientProvider>
+  </ClerkProvider>
 
 }
